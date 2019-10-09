54515909c526587ac8934c956c202a3f471b4ce7
Shaaandi <deviloversaad@gmail.com>
2019-10-09 18:27:09 +0500

    no message

---
+++ server/keys/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://deviloversaad:nQjPm0Ruf1Xhn3RO@cluster0-aa9jl.mongodb.net/test?retryWrites=true&w=majority",
+  googleClientID:
+    "583899374165-ktqf8jigt2e7ogr403bl78v82vaubf1u.apps.googleusercontent.com",
+  googleClientSecret: "n-mXcUXVHoOSY2aAulffxMrR",
+  cookieKey: "bdszfjnm4ersdzfvxcmc"
+};
