8fb4d884ed8518b940faa9584f927c42fc9b793f
Saad <saif.awan.10441@gmail.com>
2020-01-16 10:23:09 +0500

    testing deployment

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const app = express();
 const keys = require('./keys/index');
 
+console.log(keys)
+
 const corsOptions = {
   origin : keys.frontendURL,
   credentials  : true
